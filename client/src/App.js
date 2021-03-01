ffa04a6b900268654effd6491ba1d780fbdb0b2e
Soha Bhatia <soha20bhatia@gmail.com>
2021-03-01 16:05:56 +0530

    login updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 import Router from './authPages/authRouter'
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
