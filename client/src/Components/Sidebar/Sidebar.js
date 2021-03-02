6f91dfbd8eaf94368a67be4bd796a4bdb9d65caf
Anushka Shrivastava <anushka.shrivastavaa@gmail.com>
2021-03-02 09:06:37 +0530

    edits

--- client/src/Components/Sidebar/Sidebar.js
+++ client/src/Components/Sidebar/Sidebar.js
@@ -1,20 +1,17 @@
-import React from 'react'
-import '../Css/Sidebar.css'
+import React from 'react';
+import '../Css/Sidebar.css';
 import { Link } from 'react-router-dom';
 import { SidebarData } from './SidebarData';
 import Submenu from './Submenu';
 
 function Sidebar() {
-    return (
-        <div className="Sidebar_container">
-            {SidebarData.map((item,index) => {
-                return <Submenu item={item} key={index}/>
-
-               
-        })}
-            
-        </div>
-    )
+	return (
+		<div className='Sidebar_container'>
+			{SidebarData.map((item, index) => {
+				return <Submenu item={item} key={index} />;
+			})}
+		</div>
+	);
 }
 
-export default Sidebar
+export default Sidebar;
