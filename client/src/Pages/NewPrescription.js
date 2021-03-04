d5ba6f9424629db85f76d66590fc07ad44933d27
Anushka Shrivastava <anushka.shrivastavaa@gmail.com>
2021-03-04 12:21:25 +0530

    edits

--- client/src/Pages/NewPrescription.js
+++ client/src/Pages/NewPrescription.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import './pagesCSS/NewPres.css'
+import React from 'react';
+import './pagesCSS/NewPres.css';
+import {
+	FormControl,
+	InputLabel,
+	Input,
+	FormHelperText,
+} from '@material-ui/core';
 
 function NewPrescription() {
-    return (
-        <div className="NPrescription_container">
-            <div className="NP_form">
-                <h2>Add New Prescription Here</h2> 
-
-            </div>
-        </div>
-    )
+	return (
+		<div className='NPrescription_container'>
+			<h2>Add New Prescription Here</h2>
+			<div className='NP_form'>
+				<FormControl>
+					<InputLabel className='InputLabel' htmlFor='title'>Prescription Title</InputLabel>
+					<Input id='title' aria-describedby='my-helper-text' />
+					
+				</FormControl>
+			</div>
+		</div>
+	);
 }
 
-export default NewPrescription
+export default NewPrescription;
