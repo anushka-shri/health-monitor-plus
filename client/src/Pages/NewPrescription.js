b980fbede70d5a8ed080ece116a9c0d8a493eae3
Anushka Shrivastava <anushka.shrivastavaa@gmail.com>
2021-03-05 11:42:50 +0530

    edits

--- client/src/Pages/NewPrescription.js
+++ client/src/Pages/NewPrescription.js
@@ -1,27 +1,132 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import Container from '@material-ui/core/Container';
 import './pagesCSS/NewPres.css';
-import { FormControl, InputLabel, Input, Select, MenuItem } from '@material-ui/core';
 
-function NewPrescription() {
+const useStyles = makeStyles((theme) => ({
+	paper: {
+		marginTop: theme.spacing(0),
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+
+	form: {
+		width: '100%', // Fix IE 11 issue.
+		marginTop: theme.spacing(3),
+	},
+	submit: {
+		margin: theme.spacing(3, 0, 2),
+	},
+}));
+
+export default function SignUp() {
+	const classes = useStyles();
+
 	return (
 		<div className='NPrescription_container'>
-			<h2>Add New Prescription Here</h2>
-			<div className='NP_form'>
-				<FormControl className='Form_items'>
-					<InputLabel className='InputLabel' htmlFor='title'>
-						Prescription Title
-					</InputLabel>
-                    <Input id='title' aria-describedby='my-helper-text' />
-                    <div className='select_file'>
-                     <p>Scanned Documents</p>
-                    <input type="file" id="myFile" name="filename" />
-                    </div>
-                    
-  
-				</FormControl>
-			</div>
+			<Container component='main' maxWidth='xs' className='root'>
+				<CssBaseline />
+				<div className={classes.paper}>
+					<Typography component='h1' variant='h5'>
+						Add Prescription Here
+					</Typography>
+					<form className={classes.form} Validate>
+						<Grid container spacing={2}>
+							<Grid item xs={12}>
+								<TextField
+									autoComplete='title'
+									name='title'
+									variant='outlined'
+									required
+									fullWidth
+									id='title'
+									label='Prescription Title'
+									autoFocus
+								/>
+							</Grid>
+
+							<Grid item xs={12} sm={8}>
+								<TextField
+									variant='outlined'
+									required
+									fullWidth
+									id='newdoctor'
+									label='Add Doctor'
+									name='newdoc'
+									autoComplete='newdoctor'
+								/>
+							</Grid>
+							<Grid item xs={12} sm={4}>
+								<InputLabel id='demo-simple-select-label'>Doctors</InputLabel>
+								<Select value='Add Doctor'>
+									<MenuItem>AAAAa</MenuItem>
+									<MenuItem>AAggga</MenuItem>
+									<MenuItem>erereAa</MenuItem>
+								</Select>
+							</Grid>
+							<Grid item xs={12}>
+								<Button variant='contained' color='primary' sm={6}>
+									Add Doctor +
+								</Button>
+							</Grid>
+
+							<Grid item xs={12} sm={8}>
+								<TextField
+									variant='outlined'
+									required
+									fullWidth
+									id='newmeds'
+									label='Add Medicine'
+									name='newmed'
+									autoComplete='newmed'
+								/>
+							</Grid>
+							<Grid item xs={12} sm={4}>
+								<InputLabel id='demo-simple-select-label'>Medicines</InputLabel>
+								<Select>
+									<MenuItem>AAAAa</MenuItem>
+									<MenuItem>AAggga</MenuItem>
+									<MenuItem>erereAa</MenuItem>
+								</Select>
+							</Grid>
+							<Grid item xs={12}>
+								<Button variant='contained' color='primary' sm={6}>
+									Add Medicine +
+								</Button>
+							</Grid>
+							<Grid item xs={12} sm={6}>
+								<Button
+									type='submit'
+									fullWidth
+									variant='contained'
+									color='primary'
+									className={classes.submit}>
+									Save
+								</Button>
+							</Grid>
+							<Grid item xs={12} sm={6}>
+								<Button
+									type='submit'
+									fullWidth
+									variant='contained'
+									color='primary'
+									className={classes.submit}>
+									Reset
+								</Button>
+							</Grid>
+						</Grid>
+					</form>
+				</div>
+			</Container>
 		</div>
 	);
 }
-
-export default NewPrescription;
