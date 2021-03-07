2cbb767a54b00976cffd93810270ca1b4024003d
Anushka Shrivastava <anushka.shrivastavaa@gmail.com>
2021-03-08 01:52:06 +0530

    doctors page added

--- client/src/Pages/Doctors.js
+++ client/src/Pages/Doctors.js
@@ -1,61 +1,54 @@
-import React,{useState,useEffect} from 'react'
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import './trial.css'
+import React, { useState, useEffect } from 'react';
 import Particles from '../Components/Particles/Particles.js';
 import axios from 'axios';
+import './pagesCSS/Doctors.css';
+
 
 function Doctors() {
-    const [doctors, setDoctor] = useState([]);
+	const [doctors, setDoctor] = useState([]);
 
-	const getDoctors = async() => {
+	const getDoctors = async () => {
 		try {
 			const res = await axios.get(
-			  "http://localhost:3005/api/v1/doctors/getUserDoctor");
-			  //console.log(res.data.data);
+				'http://localhost:3005/api/v1/doctors/getUserDoctor',
+			);
+			//console.log(res.data.data);
 			setDoctor(res.data.data);
 			//.log(doctors);
-		   }
-		   catch {}
-	}
-    const renderDoctors = () => {
-		return doctors.map((doctor,i) => {
-			return <li key={i}>{doctor.Name}</li>
-		});
-	}
-    useEffect(() => {
+		} catch {}
+	};
+
+	useEffect(() => {
 		getDoctors();
-	},[])
+	}, []);
 
+	return (
+		<>
+			<Particles />
+			<div>
+				<h2 className='heading'>Doctors Added</h2>
+				<table className='Doctors_container'>
+					<tr>
+						<th>Doctor's Name</th>
+						<th align='right'>Hospital</th>
+						<th align='right'>Specialization</th>
+						<th align='right'>Notes</th>
+					</tr>
 
-	
-	  
-	 
-	  
-	  
-	  
-	  
-	  
-
-
-
-
-    return (
-        <>
-            <Particles />
-        <div>
-            <h2 className="heading">Doctors</h2>
-				<ul>{renderDoctors()}</ul>
-            </div>
-            </>
-    )
- 
+					{doctors.map((doctor, i) => (
+						<tr key={i}>
+							<td component='th' scope='row'>
+								{doctor.Name}
+							</td>
+							<td align='right'>{doctor.Hospital}</td>
+							<td align='right'>{doctor.Specialization}</td>
+							<td align='right'>{doctor.Notes}</td>
+						</tr>
+					))}
+				</table>
+			</div>
+		</>
+	);
 }
 
-export default Doctors
+export default Doctors;
