3f6f93400a977c341f383bfbf514f0c4e1741c31
Soha Bhatia <soha20bhatia@gmail.com>
2021-03-07 19:45:59 +0530

    update

--- client/src/Pages/Doctors.js
+++ client/src/Pages/Doctors.js
@@ -1,4 +1,12 @@
 import React,{useState,useEffect} from 'react'
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 import './trial.css'
 import Particles from '../Components/Particles/Particles.js';
 import axios from 'axios';
@@ -25,15 +33,29 @@ function Doctors() {
 		getDoctors();
 	},[])
 
+
+	
+	  
+	 
+	  
+	  
+	  
+	  
+	  
+
+
+
+
     return (
         <>
             <Particles />
         <div>
             <h2 className="heading">Doctors</h2>
-            <ul className="heading">{renderDoctors()}</ul>
+            
             </div>
             </>
     )
+ 
 }
 
 export default Doctors
