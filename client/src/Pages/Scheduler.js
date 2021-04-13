5557cf82077a8c9485c8ae6ae9a11f976a655026
Anushka Shrivastava <anushka.shrivastavaa@gmail.com>
2021-04-13 12:37:41 +0530

    scheduler form added

--- client/src/Pages/Scheduler.js
+++ client/src/Pages/Scheduler.js
@@ -1,223 +1,154 @@
+/* eslint-disable react/destructuring-assignment */
 import * as React from 'react';
 import Paper from '@material-ui/core/Paper';
-import TableCell from '@material-ui/core/TableCell';
-import FormGroup from '@material-ui/core/FormGroup';
-import Checkbox from '@material-ui/core/Checkbox';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Typography from '@material-ui/core/FormControl';
-import { makeStyles } from '@material-ui/core/styles';
-
-import {
-	ViewState,
-	EditingState,
-	IntegratedEditing,
-} from '@devexpress/dx-react-scheduler';
 import './pagesCSS/Scheduler.css';
+import { AddButton, ScForm } from './../Components';
+import { ViewState, EditingState } from '@devexpress/dx-react-scheduler';
 import {
 	Scheduler,
-	DayView,
 	WeekView,
-	MonthView,
 	Appointments,
-	AppointmentForm,
-	Toolbar,
-	DateNavigator,
-	TodayButton,
-	AppointmentTooltip,
-	ViewSwitcher,
 	DragDropProvider,
+	EditRecurrenceMenu,
+	AllDayPanel,
 } from '@devexpress/dx-react-scheduler-material-ui';
 
-import { appointments } from '../data/appointments';
-
-const useStyles = makeStyles((theme) => ({
-	container: {
-		margin: theme.spacing(2),
-		padding: theme.spacing(2),
+const recurrenceAppointments = [
+	{
+		title: 'Website Re-Design Plan',
+		startDate: new Date(2018, 5, 25, 9, 15),
+		endDate: new Date(2018, 5, 25, 11, 30),
+		id: 100,
+		rRule: 'FREQ=DAILY;COUNT=3',
+		exDate: '20180628T063500Z,20180626T061500Z',
 	},
-	text: theme.typography.h6,
-	formControlLabel: {
-		...theme.typography.caption,
-		fontSize: '1rem',
+	{
+		title: 'Book Flights to San Fran for Sales Trip',
+		startDate: new Date(2018, 5, 25, 12, 11),
+		endDate: new Date(2018, 5, 25, 13, 0),
+		id: 101,
+		rRule: 'FREQ=DAILY;COUNT=4',
+		exDate: '20180627T091100Z',
+		allDay: true,
 	},
-}));
-
-const currentDate = new Date();
-const editingOptionsList = [
-	{ id: 'allowAdding', text: 'Adding' },
-	{ id: 'allowDeleting', text: 'Deleting' },
-	{ id: 'allowUpdating', text: 'Updating' },
-	{ id: 'allowResizing', text: 'Resizing' },
-	{ id: 'allowDragging', text: 'Dragging' },
+	{
+		title: 'Install New Router in Dev Room',
+		startDate: new Date(2018, 5, 25, 13, 30),
+		endDate: new Date(2018, 5, 25, 14, 35),
+		id: 102,
+		rRule: 'FREQ=DAILY;COUNT=5',
+	},
+	{
+		title: 'Approve Personal Computer Upgrade Plan',
+		startDate: new Date(2018, 5, 26, 10, 0),
+		endDate: new Date(2018, 5, 26, 11, 0),
+		id: 3,
+		location: 'Room 2',
+	},
+	{
+		title: 'Final Budget Review',
+		startDate: new Date(2018, 5, 27, 11, 45),
+		endDate: new Date(2018, 5, 27, 13, 20),
+		id: 4,
+		location: 'Room 2',
+	},
+	{
+		title: 'New Brochures',
+		startDate: new Date(2018, 5, 26, 14, 40),
+		endDate: new Date(2018, 5, 26, 15, 45),
+		id: 5,
+		location: 'Room 2',
+	},
+	{
+		title: 'Install New Database',
+		startDate: new Date(2018, 5, 28, 9, 45),
+		endDate: new Date(2018, 5, 28, 11, 15),
+		id: 6,
+		location: 'Room 1',
+	},
+	{
+		title: 'Approve New Online Marketing Strategy',
+		startDate: new Date(2018, 5, 29, 11, 45),
+		endDate: new Date(2018, 5, 29, 13, 5),
+		id: 7,
+		location: 'Room 3',
+	},
+	{
+		title: 'Create Icons for Website',
+		startDate: new Date(2018, 5, 29, 10, 0),
+		endDate: new Date(2018, 5, 29, 11, 30),
+		id: 12,
+		location: 'Room 2',
+	},
 ];
 
-const EditingOptionsSelector = ({ options, onOptionsChange }) => {
-	const classes = useStyles();
+const dragDisableIds = new Set([3, 8, 10, 12]);
+
+const allowDrag = ({ id }) => !dragDisableIds.has(id);
+const appointmentComponent = (props) => {
+	if (allowDrag(props.data)) {
+		return <Appointments.Appointment {...props} />;
+	}
 	return (
-		<div className={classes.container}>
-			<Typography className={classes.text}>Enabled Options</Typography>
-			<FormGroup row>
-				{editingOptionsList.map(({ id, text }) => (
-					<FormControlLabel
-						control={
-							<Checkbox
-								checked={options[id]}
-								onChange={onOptionsChange}
-								value={id}
-								color='primary'
-							/>
-						}
-						classes={{ label: classes.formControlLabel }}
-						label={text}
-						key={id}
-						disabled={
-							(id === 'allowDragging' || id === 'allowResizing') &&
-							!options.allowUpdating
-						}
-					/>
-				))}
-			</FormGroup>
-		</div>
+		<Appointments.Appointment
+			{...props}
+			style={{ ...props.style, cursor: 'not-allowed' }}
+		/>
 	);
 };
 
-export default () => {
-	const [data, setData] = React.useState(appointments);
-	const [editingOptions, setEditingOptions] = React.useState({
-		allowAdding: true,
-		allowDeleting: true,
-		allowUpdating: true,
-		allowDragging: true,
-		allowResizing: true,
-	});
-	const [addedAppointment, setAddedAppointment] = React.useState({});
-	const [
-		isAppointmentBeingCreated,
-		setIsAppointmentBeingCreated,
-	] = React.useState(false);
+export default class Demo extends React.PureComponent {
+	constructor(props) {
+		super(props);
 
-	const {
-		allowAdding,
-		allowDeleting,
-		allowUpdating,
-		allowResizing,
-		allowDragging,
-	} = editingOptions;
+		this.state = {
+			data: recurrenceAppointments,
+			currentDate: new Date('2018-06-27'),
+		};
 
-	const onCommitChanges = React.useCallback(
-		({ added, changed, deleted }) => {
+		this.onCommitChanges = this.commitChanges.bind(this);
+	}
+
+	commitChanges({ added, changed, deleted }) {
+		this.setState((state) => {
+			let { data } = state;
 			if (added) {
 				const startingAddedId =
 					data.length > 0 ? data[data.length - 1].id + 1 : 0;
-				setData([...data, { id: startingAddedId, ...added }]);
+				data = [...data, { id: startingAddedId, ...added }];
 			}
 			if (changed) {
-				setData(
-					data.map((appointment) =>
-						changed[appointment.id]
-							? { ...appointment, ...changed[appointment.id] }
-							: appointment,
-					),
+				data = data.map((appointment) =>
+					changed[appointment.id]
+						? { ...appointment, ...changed[appointment.id] }
+						: appointment,
 				);
 			}
 			if (deleted !== undefined) {
-				setData(data.filter((appointment) => appointment.id !== deleted));
+				data = data.filter((appointment) => appointment.id !== deleted);
 			}
-			setIsAppointmentBeingCreated(false);
-		},
-		[setData, setIsAppointmentBeingCreated, data],
-	);
-	const onAddedAppointmentChange = React.useCallback((appointment) => {
-		setAddedAppointment(appointment);
-		setIsAppointmentBeingCreated(true);
-	});
-	const handleEditingOptionsChange = React.useCallback(({ target }) => {
-		const { value } = target;
-		const { [value]: checked } = editingOptions;
-		setEditingOptions({
-			...editingOptions,
-			[value]: !checked,
+			return { data };
 		});
-	});
+	}
 
-	const TimeTableCell = React.useCallback(
-		React.memo(({ onDoubleClick, ...restProps }) => (
-			<WeekView.TimeTableCell
-				{...restProps}
-				onDoubleClick={allowAdding ? onDoubleClick : undefined}
-			/>
-		)),
-		[allowAdding],
-	);
+	render() {
+		const { data, currentDate } = this.state;
 
-	const CommandButton = React.useCallback(
-		({ id, ...restProps }) => {
-			if (id === 'deleteButton') {
-				return (
-					<AppointmentForm.CommandButton
-						id={id}
-						{...restProps}
-						disabled={!allowDeleting}
-					/>
-				);
-			}
-			return <AppointmentForm.CommandButton id={id} {...restProps} />;
-		},
-		[allowDeleting],
-	);
-
-	const allowDrag = React.useCallback(() => allowDragging && allowUpdating, [
-		allowDragging,
-		allowUpdating,
-	]);
-	const allowResize = React.useCallback(() => allowResizing && allowUpdating, [
-		allowResizing,
-		allowUpdating,
-	]);
-
-	return (
-		<div className='meds_container'>
-			<React.Fragment>
-				{/* <EditingOptionsSelector
-				options={editingOptions}
-				onOptionsChange={handleEditingOptionsChange}
-			/> */}
+		return (
+			<div className='meds_container'>
 				<Paper className='meds_wrapper'>
-					<Scheduler data={data} height={600}>
-						<ViewState currentDate={currentDate} />
-						<Toolbar />
-						<DateNavigator />
-						<TodayButton />
-						<EditingState
-							onCommitChanges={onCommitChanges}
-							addedAppointment={addedAppointment}
-							onAddedAppointmentChange={onAddedAppointmentChange}
-						/>
-
-						<IntegratedEditing />
-						<DayView startDayHour={9} endDayHour={18} />
-						{/* <WeekView startDayHour={10} endDayHour={19} /> */}
-						<WeekView
-							startDayHour={9}
-							endDayHour={19}
-							timeTableCellComponent={TimeTableCell}
-						/>
-
-						<ViewSwitcher />
-						<Appointments />
-
-						<AppointmentTooltip
-							showOpenButton
-							showDeleteButton={allowDeleting}
-						/>
-						<AppointmentForm
-							commandButtonComponent={CommandButton}
-							readOnly={isAppointmentBeingCreated ? false : !allowUpdating}
-						/>
-						<DragDropProvider allowDrag={allowDrag} allowResize={allowResize} />
+					<Scheduler data={data} height={660}>
+						<ViewState defaultCurrentDate={currentDate} />
+						<EditingState onCommitChanges={this.onCommitChanges} />
+						<EditRecurrenceMenu />
+						<WeekView startDayHour={9} endDayHour={16} />
+						<Appointments appointmentComponent={appointmentComponent} />
+						<AllDayPanel />
+						<DragDropProvider allowDrag={allowDrag} />
 					</Scheduler>
 				</Paper>
-			</React.Fragment>
-		</div>
-	);
-};
+				<ScForm />
+			</div>
+		);
+	}
+}
--- client/src/data/appointments.js
+++ client/src/data/appointments.js
@@ -8,8 +8,8 @@ export const appointments = [
 	},
 	{
 		title: 'Book Flights to San Fran for Sales Trip',
-		startDate: new Date(2018, 5, 25, 12, 11),
-		endDate: new Date(2018, 5, 25, 13, 0),
+		startDate: new Date(2021, 4, 10, 12, 11),
+		endDate: new Date(20121, 4, 10, 13, 0),
 		id: 1,
 		location: 'Room 1',
 	},
