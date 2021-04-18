c5ee92762a3bc399ed9385172641c1568acfed29
Anushka Shrivastava <anushka.shrivastavaa@gmail.com>
2021-04-18 12:53:34 +0530

    prescriptions added css

--- client/src/Pages/Prescriptions.js
+++ client/src/Pages/Prescriptions.js
@@ -4,8 +4,6 @@ import './pagesCSS/Prescription.css';
 import * as BiIcons from 'react-icons/bi';
 import * as MdIcons from 'react-icons/md';
 
-
-
 const Prescriptions = () => {
 	const [prescriptions, setPrescription] = useState([]);
 
@@ -24,93 +22,83 @@ const Prescriptions = () => {
 		getPrescription();
 	}, []);
 
-	
 	const renderImages = (p) => {
-		 return p.map(item => 
-		 <img src={`http://localhost:3005/${item}`} alt="Girl in a jacket" width="500" height="600" />
-		 )};
-	const handleDownloadClick = (event,p) => {
-		p.map(item => {
+		return p.map((item) => (
+			<img
+				src={`http://localhost:3005/${item}`}
+				alt='Girl in a jacket'
+				width='200px'
+				height='150px'
+			/>
+		));
+	};
+	const handleDownloadClick = (event, p) => {
+		p.map((item) => {
 			var URL = `http://localhost:3005/${item}`;
-			
+
 			axios({
 				url: URL,
-				method:'GET',
-				responseType:'blob'
-
-			}).then(response => {
-				const downloadUrl = window.URL.createObjectURL(new Blob([response.data]));
-        		const link = document.createElement('a');
-        		link.href = downloadUrl;
-        		link.setAttribute('download', "prescription.png"); //any other extension
-        		document.body.appendChild(link);
-        		link.click();
-        		
+				method: 'GET',
+				responseType: 'blob',
+			}).then((response) => {
+				const downloadUrl = window.URL.createObjectURL(
+					new Blob([response.data]),
+				);
+				const link = document.createElement('a');
+				link.href = downloadUrl;
+				link.setAttribute('download', 'prescription.png'); //any other extension
+				document.body.appendChild(link);
+				link.click();
 			});
-			
-			
 		});
-	}
-  
-	return (prescriptions.map((prescription, i) => (
-		<div class='card Prescription_card' key={i}>
-			<header class='card-header'>
-				<p class='card-header-title'>Title: {prescription.Title}</p>
-			</header>
-			<div class='card-content'>
-				<div class='content'>
-					<b>Notes:</b> {prescription.Notes}
-					<br />
-					<time datetime='2016-1-1'>
-						<b>Date:  </b>
-						{prescription.DateOfRec}
-					</time>
-					{
-						renderImages(prescription.Prescription)
-					}
-				</div>
+	};
+
+	return (
+		<>
+			<div className='pres_cards_cont'>
+				<h1 className='prescription_added_heading'>Prescriptions Added</h1>
+				{prescriptions.map((prescription, i) => (
+					<div class='card' key={i}>
+						<header class='card-header'>
+							<p class='card-header-title'>Title: {prescription.Title}</p>
+						</header>
+						<div class='card-content'>
+							<div class='content'>
+								<b>Notes:</b> {prescription.Notes}
+								<br />
+								<time datetime='2016-1-1'>
+									<b>Date: </b>
+									{prescription.DateOfRec}
+								</time>
+								<div className='render_pres_card '>
+									{renderImages(prescription.Prescription)}
+								</div>
+							</div>
+						</div>
+						<footer class='card-footer'>
+							<a
+								onClick={(event) =>
+									handleDownloadClick(event, prescription.Prescription)
+								}
+								href='#'
+								class='card-footer-item'>
+								Prescription
+								<BiIcons.BiDownload className='download_icon_card' />
+							</a>
+							<a href='#' class='card-footer-item'>
+								Edit
+								<BiIcons.BiEdit className='download_icon_card' />
+							</a>
+							<a href='#' class='card-footer-item'>
+								Delete
+								<MdIcons.MdDeleteSweep className='download_icon_card' />
+							</a>
+						</footer>
+					</div>
+				))}
 			</div>
-			<footer class='card-footer'>
-				<a onClick={(event) => handleDownloadClick(event,prescription.Prescription)} href='#' class='card-footer-item'>
-					Download Presciption
-					<BiIcons.BiDownload className='download_icon_card' />
-				</a>
-				<a href='#' class='card-footer-item'>
-					Edit
-					<BiIcons.BiEdit className='download_icon_card' />
-				</a>
-				<a href='#' class='card-footer-item'>
-					Delete
-					<MdIcons.MdDeleteSweep className='download_icon_card' />
-				</a>
-			</footer>
-		</div>
-	)));
-}
+		</>
+	);
+};
 
 export default Prescriptions;
-
-
-	/* <h2 className="heading">Prescriptions</h2>
-            <table className='Doctors_container'>
-					<tr>
-						<th>Presciption Title</th>
-						<th>Doctor</th>
-						<th>Notes</th>
-                        <th>Date</th>
-						<th>Prescription</th>
-					</tr>
-
-					{prescriptions.map((prescription, i) => (
-						<tr key={i}>
-							<td>{prescription.Title}</td>
-							<td>{prescription.Doctor.Name}</td>
-							<td>{prescription.Notes}</td>
-							<td>{prescription.DateOfRec}</td>
-                            <td>{prescription.Prescription.map(element => {
-                               return <img src="./../Applogo.png" alt="Girl in a jacket" width="500" height="600"/>
-                            })}</td>
-						</tr>
-					))}
-				</table> */
-
--- server/app.js
+++ server/app.js
@@ -8,10 +8,7 @@ const doctorRoute = require('./Router/doctorRoute');
 const labReportRoute = require('./Router/labReportRoute');
 const eventsRoute = require('./Router/eventsRoute');
 const path = require('path');
-// const brain = require('brain.js');
-// const data = require('./Machine Learning/data.json');
 
-// const fs = require('fs');
 
 const app = express();
 
@@ -24,50 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
 
-// app.use((req, res, next) => {
- 
-// // create a simple feed forward neural network with backpropagation
-//   const net = new brain.recurrent.LSTM();
-   
-//   //var objs = JSON.parse(data);
-//   const obj = JSON.parse(fs.readFileSync(`${__dirname}/Machine Learning/data.json`, `utf-8`));
-  
-//  const trainData = obj.map(item =>(
-//     {
-//       input:[item.Symptom_1,item.Symptom_2,item.Symptom_3,item.Symptom_4,item.Symptom_5,item.Symptom_6,item.Symptom_7,item.Symptom_8,item.Symptom_9,item.Symptom_10,item.Symptom_11,item.Symptom_12,item.Symptom_13,item.Symptom_14,item.Symptom_15,item.Symptom_16,item.Symptom_17],
-//         //Symptom_4,item.Symptom_5,item.Symptom_6,item.Symptom_7,item.Symptom_8,item.Symptom_9,item.Symptom_10,item.Symptom_11,item.Symptom_12,item.Symptom_13,item.Symptom_14,item.Symptom_15,item.Symptom_16,item.Symptom_17],
-//       //input:['happy','sad'],
-//       output: [item.Disease],
-//     }
-//   ));
- 
-//   // const trainData = [
-//   //   {input:'I am happy', output:'happy'},
-//   //   {input:'I am sad', output:'sad'},
-//   //   {input:'what a pill' , output:'sarcastic'}
-//   // ];
-// //console.log(trainData);
-//   net.train(trainData,{
-//     iterations:200,
-//     //log:(stats) => console.log(stats)
-//   });
-
-//   //const output = net.run(['happy']);
-//   const output = net.run(['itching','skin_rash','nodal_skin_eruptions',"dischromic _patches"
-// ]);
-//   console.log(output);
-//   next();
-// });
-
-
-
-
-
-
-
-
-
-//Mounting our new routers to app on a specified route
 
 app.use("/api/v1/users",userRouter);
 app.use("/api/v1/monitor",monitorRoute);
