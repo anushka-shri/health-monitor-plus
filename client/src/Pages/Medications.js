75762cebad68551413cd8f30ec61b7807fd3062f
Anushka Shrivastava <anushka.shrivastavaa@gmail.com>
2021-05-08 16:39:28 +0530

    cards added

--- client/src/Pages/Medications.js
+++ client/src/Pages/Medications.js
@@ -23,33 +23,34 @@ export default function Medications() {
 					)}
 				/>
 			</div>
-
-			<Card.Group>
-				<Card>
-					<Card.Content>
-						<Image
-							floated='right'
-							size='mini'
-							src='/images/avatar/large/jenny.jpg'
-						/>
-						<Card.Header>Jenny Lawrence</Card.Header>
-						<Card.Meta>New User</Card.Meta>
-						<Card.Description>
-							Jenny requested permission to view your contact details
-						</Card.Description>
-					</Card.Content>
-					<Card.Content extra>
-						<div className='ui two buttons'>
-							<Button basic color='green'>
-								Approve
-							</Button>
-							<Button basic color='red'>
-								Decline
-							</Button>
-						</div>
-					</Card.Content>
-				</Card>
-			</Card.Group>
+			<div className='Meds_cards_cont'>
+				<Card.Group>
+					<Card>
+						<Card.Content>
+							<Image
+								floated='right'
+								size='mini'
+								src='/images/avatar/large/jenny.jpg'
+							/>
+							<Card.Header>Jenny Lawrence</Card.Header>
+							<Card.Meta>New User</Card.Meta>
+							<Card.Description>
+								Jenny requested permission to view your contact details
+							</Card.Description>
+						</Card.Content>
+						<Card.Content extra>
+							<div className='ui two buttons'>
+								<Button basic color='green'>
+									Approve
+								</Button>
+								<Button basic color='red'>
+									Decline
+								</Button>
+							</div>
+						</Card.Content>
+					</Card>
+				</Card.Group>
+			</div>
 		</div>
 	);
 }
@@ -128,3 +129,5 @@ const top100Films = [
 	{ title: 'Taxi Driver', year: 1976 },
 	{ title: 'Lawrence of Arabia', year: 1962 },
 ];
+
+
