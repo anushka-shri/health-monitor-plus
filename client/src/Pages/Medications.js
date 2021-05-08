9e7b150cf063adcfb5e3f3f5c0357d709026018d
Anushka Shrivastava <anushka.shrivastavaa@gmail.com>
2021-05-08 16:25:08 +0530

    searchbar and cards

--- client/src/Pages/Medications.js
+++ client/src/Pages/Medications.js
@@ -1,12 +1,130 @@
+/* eslint-disable no-use-before-define */
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { Button, Card, Image } from 'semantic-ui-react';
+import { Autocomplete } from '@material-ui/lab';
+import './pagesCSS/Medications.css';
 
-function Error() {
+export default function Medications() {
 	return (
 		<div>
-			<h2 className='heading'> Error</h2>
-			<h5 className='heading'>Page not found</h5>
+			<div className='Meds_search_cont' style={{ width: 400 }}>
+				<Autocomplete
+					id='free-solo-demo'
+					freeSolo
+					options={top100Films.map((option) => option.title)}
+					renderInput={(params) => (
+						<TextField
+							{...params}
+							label='Search Meds'
+							margin='normal'
+							variant='outlined'
+						/>
+					)}
+				/>
+			</div>
+
+			<Card.Group>
+				<Card>
+					<Card.Content>
+						<Image
+							floated='right'
+							size='mini'
+							src='/images/avatar/large/jenny.jpg'
+						/>
+						<Card.Header>Jenny Lawrence</Card.Header>
+						<Card.Meta>New User</Card.Meta>
+						<Card.Description>
+							Jenny requested permission to view your contact details
+						</Card.Description>
+					</Card.Content>
+					<Card.Content extra>
+						<div className='ui two buttons'>
+							<Button basic color='green'>
+								Approve
+							</Button>
+							<Button basic color='red'>
+								Decline
+							</Button>
+						</div>
+					</Card.Content>
+				</Card>
+			</Card.Group>
 		</div>
 	);
 }
 
-export default Error;
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+const top100Films = [
+	{ title: 'The Shawshank Redemption', year: 1994 },
+	{ title: 'The Godfather', year: 1972 },
+	{ title: 'The Godfather: Part II', year: 1974 },
+	{ title: 'The Dark Knight', year: 2008 },
+	{ title: '12 Angry Men', year: 1957 },
+	{ title: "Schindler's List", year: 1993 },
+	{ title: 'Pulp Fiction', year: 1994 },
+	{ title: 'The Lord of the Rings: The Return of the King', year: 2003 },
+	{ title: 'The Good, the Bad and the Ugly', year: 1966 },
+	{ title: 'Fight Club', year: 1999 },
+	{ title: 'The Lord of the Rings: The Fellowship of the Ring', year: 2001 },
+	{ title: 'Star Wars: Episode V - The Empire Strikes Back', year: 1980 },
+	{ title: 'Forrest Gump', year: 1994 },
+	{ title: 'Inception', year: 2010 },
+	{ title: 'The Lord of the Rings: The Two Towers', year: 2002 },
+	{ title: "One Flew Over the Cuckoo's Nest", year: 1975 },
+	{ title: 'Goodfellas', year: 1990 },
+	{ title: 'The Matrix', year: 1999 },
+	{ title: 'Seven Samurai', year: 1954 },
+	{ title: 'Star Wars: Episode IV - A New Hope', year: 1977 },
+	{ title: 'City of God', year: 2002 },
+	{ title: 'Se7en', year: 1995 },
+	{ title: 'The Silence of the Lambs', year: 1991 },
+	{ title: "It's a Wonderful Life", year: 1946 },
+	{ title: 'Life Is Beautiful', year: 1997 },
+	{ title: 'The Usual Suspects', year: 1995 },
+	{ title: 'Léon: The Professional', year: 1994 },
+	{ title: 'Spirited Away', year: 2001 },
+	{ title: 'Saving Private Ryan', year: 1998 },
+	{ title: 'Once Upon a Time in the West', year: 1968 },
+	{ title: 'American History X', year: 1998 },
+	{ title: 'Interstellar', year: 2014 },
+	{ title: 'Casablanca', year: 1942 },
+	{ title: 'City Lights', year: 1931 },
+	{ title: 'Psycho', year: 1960 },
+	{ title: 'The Green Mile', year: 1999 },
+	{ title: 'The Intouchables', year: 2011 },
+	{ title: 'Modern Times', year: 1936 },
+	{ title: 'Raiders of the Lost Ark', year: 1981 },
+	{ title: 'Rear Window', year: 1954 },
+	{ title: 'The Pianist', year: 2002 },
+	{ title: 'The Departed', year: 2006 },
+	{ title: 'The Great Dictator', year: 1940 },
+	{ title: 'Cinema Paradiso', year: 1988 },
+	{ title: 'The Lives of Others', year: 2006 },
+	{ title: 'Grave of the Fireflies', year: 1988 },
+	{ title: 'Paths of Glory', year: 1957 },
+	{ title: 'Django Unchained', year: 2012 },
+	{ title: 'The Shining', year: 1980 },
+	{ title: 'WALL·E', year: 2008 },
+	{ title: 'American Beauty', year: 1999 },
+	{ title: 'The Dark Knight Rises', year: 2012 },
+	{ title: 'Princess Mononoke', year: 1997 },
+	{ title: 'Aliens', year: 1986 },
+	{ title: 'Oldboy', year: 2003 },
+	{ title: 'Once Upon a Time in America', year: 1984 },
+	{ title: 'Witness for the Prosecution', year: 1957 },
+	{ title: 'Das Boot', year: 1981 },
+	{ title: 'Citizen Kane', year: 1941 },
+	{ title: 'North by Northwest', year: 1959 },
+	{ title: 'Vertigo', year: 1958 },
+	{ title: 'Star Wars: Episode VI - Return of the Jedi', year: 1983 },
+	{ title: 'Reservoir Dogs', year: 1992 },
+	{ title: 'Braveheart', year: 1995 },
+	{ title: 'M', year: 1931 },
+	{ title: 'Requiem for a Dream', year: 2000 },
+	{ title: 'Amélie', year: 2001 },
+	{ title: 'A Clockwork Orange', year: 1971 },
+	{ title: 'Like Stars on Earth', year: 2007 },
+	{ title: 'Taxi Driver', year: 1976 },
+	{ title: 'Lawrence of Arabia', year: 1962 },
+];
